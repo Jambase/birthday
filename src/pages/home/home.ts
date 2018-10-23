81b0e77a7cecac299764f8eef3e29f69a9638915
Lwazi Tongo <jambilwah@gmail.com>
2018-10-23 13:36:44 +0200

    initail commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
 
+ 
+
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Happy Birthday!',
+      subTitle: 'As you go through each year, remember to count your blessings, not your age. Count your amazing experiences, not your mistakes. Happy Birthday to an insanely awesome person!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
